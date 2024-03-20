8b1e663251bbf9c33dc979ade7e3fb42e3ac697a
LegendFabian4 <121681426+LegendFabian4@users.noreply.github.com>
2024-03-20 11:17:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function button11old() {
+    document.getElementById("button1").innerHTML = "Dont click on this"
+}
+function button11new() {
+    try {
+        document.getElementById("button1").innerHTML = "Dont"
+    }
+    catch (err11new) {
+        console.error("Error: ", err11new)
+    }
+}
+function button12() {
+    try {
+        document.getElementById("button1").innerHTML = "Are you serious right now"
+        window.open("index1.html")
+    }
+    catch (err12) {
+        console.error("Error: ", err12)
+    }
+}
