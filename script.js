616a1f9c99e523670262e7d4cc7539771e3e519a
LegendFabian4 <121681426+LegendFabian4@users.noreply.github.com>
2024-04-03 11:43:28 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+num1 = 0
+
+
+function main() {
+
+}
+function easteregg(eastereggnum1){
+    if (eastereggnum1 == 1){
+        window.open("https://www.drummencustomguns.com")
+    }
+}
+
 function button11old() {
     document.getElementById("button1").innerHTML = "Dont click on this please"
 }
@@ -11,10 +23,23 @@ function button11new() {
 }
 function button12() {
     try {
-        document.getElementById("button1").innerHTML = "Are you serious right now"
-        window.open("index1.html")
+        while(true){
+            if (num1 == 4){
+                easteregg(1)
+                num1 = num1 + 100
+                console.log(num1)
+            }
+            else{
+                document.getElementById("button1").innerHTML = "Are you serious right now"
+                window.open("index1.html")
+                num1 = num1 + 1
+                console.log(num1)
+                return
+            }
+        }
+        
     }
-    catch (err12) {
+        catch (err12) {
         console.error("Error: ", err12)
     }
 }
